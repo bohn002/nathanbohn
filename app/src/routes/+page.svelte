cc743152d16be216e6e86fcab844ff7dd5b1ca6e
Nate Bohn <nathanbohn@gmail.com>
2023-08-30 07:24:53 -0500

    make it pretty

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-	import { Section, News, HeroHeader, HeroBody } from 'flowbite-svelte-blocks';
-	import { Button } from 'flowbite-svelte';
+	import { Section, HeroHeader, HeroBody } from 'flowbite-svelte-blocks';
+	import { Button, Span } from 'flowbite-svelte';
 	import { Icon } from 'flowbite-svelte-icons';
 </script>
 
 <Section name="heroDefault">
 	<HeroHeader>
-		<svelte:fragment slot="h1">Hi! I'm Nate!</svelte:fragment>
+		<svelte:fragment slot="h1">Hi! I'm <Span gradient>Nate Bohn!</Span></svelte:fragment>
 		<svelte:fragment slot="paragraph"
 			>I am a Full-Stack/DevOps/Platform Engineer who really enjoys Beards, Root Beer, Food, Tech,
 			Dad-jokes and 3d-printing!
