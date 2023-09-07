78b701bc64f3b83900742c31a3491d5db0413e86
Nate Bohn <nathanbohn@gmail.com>
2023-09-06 21:42:37 -0500

    more fun icons

--- app/src/lib/components/Navigation/Sidebar.svelte
+++ app/src/lib/components/Navigation/Sidebar.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from '$app/stores';
 	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
-	import { Icon } from 'flowbite-svelte-icons';
+	import Icon from '@iconify/svelte';
 	$: activeUrl = $page.url.pathname;
 	let activeClass =
 		'flex items-center p-2 text-base font-normal text-primary-900 bg-primary-200 dark:bg-primary-700 rounded-lg dark:text-white hover:bg-primary-100 dark:hover:bg-gray-700';
@@ -14,7 +14,7 @@
 		<SidebarGroup>
 			<SidebarItem label="Home" href="/">
 				<svelte:fragment slot="icon">
-					<Icon name="home-solid" class="w-5 h-5" />
+					<Icon icon="mdi:home" class="w-5 h-5" />
 				</svelte:fragment>
 			</SidebarItem>
 			<!-- <SidebarItem label="Resume" href="/resume">
@@ -24,7 +24,7 @@
 			</SidebarItem> -->
 			<SidebarItem label="3D Printing" href="/3d-printing">
 				<svelte:fragment slot="icon">
-					<Icon name="draw-square-solid" class="w-5 h-5" />
+					<Icon icon="mdi:printer-3d-nozzle" class="w-5 h-5" />
 				</svelte:fragment>
 			</SidebarItem>
 			<!-- <SidebarItem label="Notes" href="/notes">
@@ -34,7 +34,7 @@
 			</SidebarItem> -->
 			<SidebarItem label="Nifty Links" href="/links">
 				<svelte:fragment slot="icon">
-					<Icon name="link-solid" class="w-5 h-5" />
+					<Icon icon="mingcute:globe-fill" class="w-5 h-5" />
 				</svelte:fragment>
 			</SidebarItem>
 		</SidebarGroup>
